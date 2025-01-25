c14fcbccdc96cbfe8d07e610a24760368ebe17f4
Eduard Davletov <eduarddavletov@Mac-mini-Eduard.local>
2025-01-25 23:11:38 +0200

    started page logic

--- src/component/LoginComponent.jsx
+++ src/component/LoginComponent.jsx
@@ -2,7 +2,30 @@ import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function LoginComponent(){
-    
+    const email = useRef();
+    const password = useRef();
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+
+    }
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Email</label>
+                    <input type="text" ref={email} required> </input>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" ref={password} required> </input>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default LoginComponent;
